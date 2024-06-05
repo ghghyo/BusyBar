6ccac4fe4fc0aada27a8ceda0571d6940cc4d71f
ghghyo <youssef.abubaker@outlook.com>
2024-06-05 15:52:52 -0600

    new ip address

--- src/pages/api/location.ts
+++ src/pages/api/location.ts
@@ -9,18 +9,19 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   // Extract the client's IP address from headers
-  const ip = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.socket.remoteAddress;
+  const forwarded = req.headers['x-forwarded-for'];
+  const ip = typeof forwarded === 'string' ? forwarded.split(/, /)[0] : req.socket.remoteAddress;
+
   console.log(`Client IP: ${ip}`); // Debugging line to log the IP
 
   try {
     // Use the client's IP address in the API request
     const response = await fetch(`https://ipinfo.io/json?token=689fae9f921906`, {
       headers: {
-        'Authorization': `689fae9f921906`
+        'Authorization': `Bearer 689fae9f921906`
       }
     });
     if (response.ok) {
-      console.log(req.headers['x-forwarded-for'])
       const data = await response.json();
       res.status(200).json({ city: data.city });
     } else {
